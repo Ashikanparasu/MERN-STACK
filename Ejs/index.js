414c25118ff2d2395c304f3baafb4c057c13f0c0
Ashikanparasu <ashikanparasu@gmail.com>
2025-12-02 22:02:37 +0530

    Ejs

--- Ejs/index.js
+++ Ejs/index.js
@@ -6,6 +6,11 @@ const app = express();
 
 let port = 8080;
 
+app.set("view engine","ejs");
+
+app.get("/",(req,res) => {
+    res.render("home.ejs");
+});
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
