228dd6cf242794878100784965e9920648ba7f9c
Ashikanparasu <ashikanparasu@gmail.com>
2025-12-04 02:57:42 +0530

    SqlClass

---
+++ Sqlclass/index.js
@@ -0,0 +1,34 @@
+const { faker } = require('@faker-js/faker');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'college',
+  password: '2244Ashik@',
+});
+
+// A simple SELECT query
+try {
+connection.query("SHOW TABLES",(err, result) => {
+    if (err) throw err;
+    console.log(result); // results contains rows returned by server
+    // fields contains extra meta data about results, if available
+  });
+} catch(err){
+    console.log(err);
+}
+
+connection.end();
+
+let createRandomUser = () => {
+  return {
+    userId: faker.string.uuid(),
+    username: faker.internet.username(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+   
+  };
+};
+
+console.log(createRandomUser());
